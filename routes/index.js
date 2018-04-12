adedac2ba5dc923d09eb026a06d1b2a4091d8825
thedon122 <donovan122@yahoo.com>
2018-04-12 11:42:32 -0400

    modfityed home page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {sPartyName:'Enter party name here'}, {sHostName:'Enter host name here'}, {hostName:'Enter host name here'} );
+  res.render('index', {sPartyName:'Enter party name here'});
 });
 
 module.exports = router;
