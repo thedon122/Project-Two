afad563871e9e86822c184db36168779c77f881d
thedon122 <donovan122@yahoo.com>
2018-04-11 23:01:30 -0400

    add connection for mongoose database

--- server.js
+++ server.js
@@ -10,6 +10,17 @@ var usersRouter = require('./routes/users');
 
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.once('open', () =>{
+  console.log('Mongoose has connected to MongoDB!')
+} )
+
+mongoose.connection.on('error', (error) => {
+  console.error(`
+  MongoDB connection error!!!
+  ${error}
+  `)
+  process.exit(-1)
+})
 
 var app = express();
 
