aef096ebe008777c7bc35a4912a4d15ee02a34cc
thedon122 <donovan122@yahoo.com>
2018-04-12 11:21:28 -0400

    modfityed home page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cosplay Party Time' });
+  res.render('index', { sPartyName: 'Enter party name here' });
 });
 
 module.exports = router;
