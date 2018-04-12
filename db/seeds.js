afd88ee7a896571639581bb8e0d76edae8cd6cb8
thedon122 <donovan122@yahoo.com>
2018-04-12 14:13:12 -0400

    added connection to mongo database

---
+++ db/seeds.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+// connect to database
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.open('open', () => {
+    console.log(`Mongoose has connected to MongoDB`)
+  })
+  
+  mongoose.connection.on('error', (error) => {
+    console.error(`
+    MongoDB connection error!!!
+    ${error}`)
+    process.exit(-1)
+  })
--- server.js
+++ server.js
@@ -11,6 +11,16 @@ var usersRouter = require('./routes/users');
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI);
 
+mongoose.connection.open('open', () => {
+  console.log(`Mongoose has connected to MongoDB`)
+})
+
+mongoose.connection.on('error', (error) => {
+  console.error(`
+  MongoDB connection error!!!
+  ${error}`)
+  process.exit(-1)
+})
 var app = express();
 
 // view engine setup
