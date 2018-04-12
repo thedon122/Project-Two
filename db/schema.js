f4d2f970e2eccb3e03a186d9017ad1a93a597f9b
thedon122 <donovan122@yahoo.com>
2018-04-12 12:05:27 -0400

    created schema for host

--- db/schema.js
+++ db/schema.js
@@ -3,3 +3,16 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 // creating a promise that can be used thourgh out the application
 mongoose.Promise = global.Promise
+
+const hostSchema = new Schema({ 
+        name: {type: String,
+            required: [true, 'Host name is required']
+        },
+        contactEmail: {type: String
+        },
+        contactNumber: {type: String
+        },
+        pictureUrl: {
+            type: String
+        } 
+    })
