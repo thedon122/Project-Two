e06865b9231d85b4d5c3c5a27d0574022f1a7449
thedon122 <donovan122@yahoo.com>
2018-04-13 17:36:30 -0400

    about to delate routes folder

--- controllers/partyController.js
+++ controllers/partyController.js
@@ -4,18 +4,55 @@ const router = express.Router({ mergeParams: true })
 const express = require('express')
 const router = express.Router()
 const Party = require('../models/party.js')
-// route to search for party names
-router.get('/:partyID', (request, response) => {
-    const partyID = request.params.id
-    Party.findById(partyID)
-    .then((party) => {
-        // renders a list of parties
-        response.render('party/show', {
-            Party,
-            pageTitle: 'List of parities'
+// route to display all parties
+router.get('/', (request, response) => {
+    User.find({})
+        .then((partys) => {
+            response.render('users/index', {
+                partys,
+                pageTitle: 'All Parties'
+            })
         })
         .catch((error) => {
             console.log(error)
         })
+})
+
+// route to search for host names
+router.get('/:hostID', (request, response) => {
+    const hostID = request.params.hostID
+    Host.findById(hostID)
+        .then((host) => {
+            // renders a list of partiess with that name
+            response.render('partys/show', {
+                host,
+                pageTitle: 'List of host'
+            })
+                .catch((error) => {
+                    console.log(error)
+                })
+        })
+})
+
+router.get('/:hostID', (request, response) => {
+    const hostID = request.params.hostID
+    response.render('cantfind', {
+        hostID
     })
+        .catch((error) => {
+            console.log(error)
+        })
+        .then((host) => {
+            User.find({})
+                .then((partys) => {
+                    response.render('users/index', {
+                        partys,
+                        pageTitle: 'All Host'
+                    })
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+        })
+
 })
