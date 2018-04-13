e7c0d772730d9631878ef37ef694ffe017524305
thedon122 <donovan122@yahoo.com>
2018-04-13 17:06:15 -0400

    modfited controller to load all host

--- controllers/hostController.js
+++ controllers/hostController.js
@@ -3,6 +3,20 @@ import { request } from 'https';
 const express = require('express')
 const router = express.Router()
 const Host = require('../models/host.js')
+// route to display all host
+router.get('/', (request, response) => {
+    User.find({})
+        .then((hosts) => {
+            response.render('users/index', {
+                hosts,
+                pageTitle: 'All Host'
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
+
 // route to search for host names
 router.get('/:hostID', (request, response) => {
     const hostID = request.params.hostID
@@ -13,8 +27,31 @@ router.get('/:hostID', (request, response) => {
                 host,
                 pageTitle: 'List of host'
             })
-            .catch((error) => {
-                console.log(error)
-            })
+                .catch((error) => {
+                    console.log(error)
+                })
         })
+})
+
+router.get('/:hostID', (request, response) => {
+    const hostID = request.params.hostID
+    response.render('cantfind', {
+        hostID
+    })
+        .catch((error) => {
+            console.log(error)
+        })
+        .then((host) => {
+            User.find({})
+                .then((hosts) => {
+                    response.render('users/index', {
+                        hosts,
+                        pageTitle: 'All Host'
+                    })
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+        })
+
 })
